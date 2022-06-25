030e7e61755c7c564207b020b9d8dae205fa46ff
Loretac <LC@vt.edu>
2022-06-24 22:31:50 -0500

    Initial Commit

---
+++ ElegantWeather-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElegantWeather-iOS
+//
+//  Created by Chris Loreta on 6/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
